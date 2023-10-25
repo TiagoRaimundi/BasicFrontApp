6ece084e409c1c051edb54e7afb066b09cc98720
Tiago Raimundi <88736718+Tiagoyeh@users.noreply.github.com>
2023-10-25 16:10:31 -0300

    65

--- src/screens/app/CreateListin/index.js
+++ src/screens/app/CreateListin/index.js
@@ -1,26 +1,46 @@
 import React, { useState } from 'react';
-import { ScrollView, Text, TouchableOpacity, } from 'react-native';
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { styles } from './styles';
+import { launchImageLibrary } from 'react-native-image-picker';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from '../../../components/header/index';
 
-
 const CreateListin = ({ navigation }) => {
+    const [images, setImages] = useState([])
 
     const goBack = () => {
         navigation.goBack();
     }
 
+    const UploadNewImage = async () => {
+        const result = await launchImageLibrary();
+        console.log('resul :>>>', result);
+        if (result?.assets?.length) {
+            setImages(prevImages => [...prevImages, ...result.assets]);
+        }
+    }
 
+    console.log('images:>>', images);
 
     return (
         <SafeAreaView>
             <Header showBack={true} onBackPress={goBack} title="Create a new Listing" />
             <ScrollView style={styles.container}>
-                <Text style={styles.sectionTitle}>Upload Photos</Text>
-                <TouchableOpacity>
-                    <Text>+</Text>
-                </TouchableOpacity>
+            <Text style={styles.sectionTitle}>Upload Photos</Text>
+
+                <View style={styles.imageRow}>
+               
+                    <TouchableOpacity style={styles.uploadContainer} onPress={UploadNewImage}>
+                        <View style={styles.uploadCircle}>
+                            <Text style={styles.uploadPlus}>+</Text>
+                        </View>
+                    </TouchableOpacity>
+                    {images.map(image => (
+                        <Image key={image.fileName} style={styles.image} source={{ uri: image.uri }} />
+                    ))}
+
+                </View>
+
             </ScrollView>
         </SafeAreaView>
     )
--- src/screens/app/CreateListin/styles.js
+++ src/screens/app/CreateListin/styles.js
@@ -7,9 +7,47 @@ export const styles = StyleSheet.create({
     },
     sectionTitle: {
         fontWeight: '500',
-        fontSize: 16,
+        fontSize: 14,
         color: colors.blue,
-        marginBottom: 16
+        marginBottom: 16,
     },
-   
+    image: {
+        width: 100,
+        height: 100,
+        borderRadius: 4,
+    },
+    uploadContainer: {
+        width: 100,
+        height: 100,
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: colors.grey,
+        borderStyle: 'dotted',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 8,
+        marginTop: 8,
+    },
+    uploadCircle: {
+        width: 32,
+        height: 32,
+        borderRadius: 20,
+        backgroundColor: colors.lightGrey,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    uploadPlus: {
+        color: colors.white,
+        fontSize: 28,
+        marginTop: -4,
+    },
+    imageRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+        paddingBottom: 16,
+    },
+  
 })
